3e50e2daeb189df3fedf8cc3aa1e75271db17a50
Gerard Kamran Kalambacal <kamrangerardkalambacal@gmail.com>
2023-08-21 17:16:20 +0800

    refactor: changed Input class name to WordFrequencyInfo

--- src/main/java/WordFrequencyInfo.java
+++ src/main/java/WordFrequencyInfo.java
@@ -1,10 +1,10 @@
-public class Input {
+public class WordFrequencyInfo {
     private String value;
     private int count;
 
-    public Input(String w, int i){
-        this.value =w;
-        this.count =i;
+    public WordFrequencyInfo(String w, int i) {
+        this.value = w;
+        this.count = i;
     }
 
 
